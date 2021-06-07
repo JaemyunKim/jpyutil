7f571f871a8eaa7a8cee35f3e9685b77464f1f54
Jaemyun Kim <JaemyunKim@users.noreply.github.com>
2021-06-07 12:28:22 +0900

    change usage

--- s3_upload_file.py
+++ s3_upload_file.py
@@ -7,20 +7,17 @@ import multiprocessing
 
 def main():
 	# read and set variables for s3 storage
-	env_items = readEnv(filename="env")
+	env_items = readEnv(filename=".env")
 	env_items["BUCKET_NAME"] = "dataset" # S3 bucket name
 	env_items["PREFIX"] = "db_prefix/db_dataset"  # destination path
 	env_items["LOCAL_DIRECTORY"] = "./test" # source directory
-	env_items["NUM_CPUS"] = multiprocessing.cpu_count() - 1
+	env_items["NUM_PROCESS"] = 1 * multiprocessing.cpu_count() - 1
 
-	# get a filename list
-	files = getFilelist(env_items["LOCAL_DIRECTORY"])
-	if len(files) == 0:
-		exit(0)
-
-	# upload files
+	# generate a filename list and upload them
 	s3_storage = S3Storage(env_items)
-	s3_storage.upload_files(files)
+	if s3_storage.upload_files() is False:
+            print("Failed to run S3Storage!!")
+            exit(1)
 
 
 if __name__ == "__main__":
