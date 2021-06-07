d1e218ba8b2377de5e246e37d64307e5160e4c71
Jaemyun Kim <JaemyunKim@users.noreply.github.com>
2021-06-07 12:47:10 +0900

    fix indentation

--- s3_upload_file.py
+++ s3_upload_file.py
@@ -6,21 +6,21 @@ import multiprocessing
 
 
 def main():
-	# read and set variables for s3 storage
-	env_items = readEnv(filename=".env")
-	env_items["BUCKET_NAME"] = "dataset" # S3 bucket name
-	env_items["PREFIX"] = "db_prefix/db_dataset"  # destination path
-	env_items["LOCAL_DIRECTORY"] = "./test" # source directory
-	env_items["NUM_PROCESS"] = 1 * multiprocessing.cpu_count() - 1
+    # read and set variables for s3 storage
+    env_items = readEnv(filename=".env")
+    env_items["BUCKET_NAME"] = "dataset" # S3 bucket name
+    env_items["PREFIX"] = "db_prefix/db_dataset"  # destination path
+    env_items["LOCAL_DIRECTORY"] = "./test" # source directory
+    env_items["NUM_PROCESS"] = 1 * multiprocessing.cpu_count() - 1
 
-        log_dir = "logs"
+    log_dir = "logs"
 
-	# generate a filename list and upload them
-	s3_storage = S3Storage(env_items, log_dir=log_dir)
-	if s3_storage.upload_files() is False:
-            print("Failed to run S3Storage!!")
-            exit(1)
+    # generate a filename list and upload them
+    s3_storage = S3Storage(env_items, log_dir=log_dir)
+    if s3_storage.upload_files() is False:
+        print("Failed to run S3Storage!!")
+        exit(1)
 
 
 if __name__ == "__main__":
-	main()
+    main()
